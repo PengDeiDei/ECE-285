c1d814377320bff965c438fc8f37300cca9d8809
hap045 <Hap045@ucsd.edu>
2022-04-20 16:42:07 -0700

    Update loss_func.py

--- assignment1/ece285/layers/loss_func.py
+++ assignment1/ece285/layers/loss_func.py
@@ -15,7 +15,8 @@ class CrossEntropyLoss(BaseLayer):
         N, _ = input_x.shape
         # Calculate the sum of losses for each example, loss for one example -log(e_i/sum(e_j)) where i is the
         # correct class according to the label target_y and j is sum over all classes
-        # loss =
+        y_one_hot = np.eye(_)[target_y]
+        loss = -np.sum(np.sum(y_one_hot * np.log(input_x)))
         # Normalize the loss by dividing by the total number of samples N
         loss /= N
         # Store your loss output and input and targets in cache
@@ -32,7 +33,8 @@ class CrossEntropyLoss(BaseLayer):
 
         # Use the formula for the gradient of Cross entropy loss to calculate the gradients
         # Gradient matrix will be NxD matrix, with N rows for all the samples in the minibatch, and D=3072
-        # dx =
+        y_truth = np.eye(_)[y_temp]
+        dx = x_temp - y_truth
         assert x_temp.shape == dx.shape, "Mismatch in shape"
         # Normalize the gradient by dividing with the total number of samples N
         dx /= N
